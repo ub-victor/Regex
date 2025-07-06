b1ceac0ad4126f9b3d02091e3157a153d5378d98
ub-victor <victoireushindi371@gmail.com>
2025-07-07 01:24:50 +0200

    Initial commit

--- validation.js
+++ validation.js
@@ -5,7 +5,8 @@ const inputs = document.querySelectorAll('input');
 const patterns = {
     telephone: /^\d{10}$/,
     username: /^[a-z\d]{5,12}$/i,
-    password: /^[\w@-]{8-20}$/ // Alphanumeric characters, @, _, and - are allowed but not special characters like !, $, %, etc.
+    password: /^[\w@-]{8,20}$/, // Alphanumeric characters, @, _, and - are allowed but not special characters like !, $, %, etc. eg: password123
+    slug: /^[a-z\d-]{8,20}$/
 }
 
 //Validation function
