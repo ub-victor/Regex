a4587384c5ea827cc4c32115f3c770af5ee7a89f
ub-victor <victoireushindi371@gmail.com>
2025-07-06 19:04:16 +0200

    Initial commit

--- validation.js
+++ validation.js
@@ -8,14 +8,23 @@ const patterns = {
 
 //Validation function
 
-function validation(field, regex){
-   console.log(regex.test(field.value));
+function validate(field, regex){
+   // console.log(regex.test(field.value));// Check if the field value matches the regex pattern 
+   if(regex.test(field.value)){
+    field.className="valid";
+   }else{
+    field.className="invalid"
+   }
 }
 
 
 inputs.forEach((input)=>{
+    // here the logic is to get the input field and its name attribute, then use that name to get the corresponding regex pattern from the patterns object.
+    // Then we can use that regex pattern to validate the input field value.
+    // The event listener is added to the input field to listen for the keyup event,
+    
     input.addEventListener("keyup", (e)=>{
         //console.log(e.target.attributes.name.value);
-        validate(e.target, [e.target.attributes.name.value]);
+        validate(e.target, patterns[e.target.attributes.name.value]); // the e.target = field and field.value = input value
     })
 })
